7a0032fda831d39206a9d5000242d8c950f2e076
marilia gabriela <rocha.marilia@escolar.ifrn.edu.br>
2022-09-01 17:23:45 -0300

    parte 5 concluída

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -29,37 +29,26 @@ class QuestionModelTests(TestCase):
     return Question.objects.create(question_text=question_text, pub_date=time)
 
 
-class QuestionIndexViewTests(TestCase):
-    def test_no_questions(self):
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
         """
-        If no questions exist, an appropriate message is displayed.
+        The detail view of a question with a pub_date in the future
+        returns a 404 not found.
         """
-        response = self.client.get(reverse('polls:index'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "No polls are available.")
-        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+        future_question = create_question(question_text='Future question.', days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
 
     def test_past_question(self):
         """
-        Questions with a pub_date in the past are displayed on the
-        index page.
+        The detail view of a question with a pub_date in the past
+        displays the question's text.
         """
-        question = create_question(question_text="Past question.", days=-30)
-        response = self.client.get(reverse('polls:index'))
-        self.assertQuerysetEqual(
-            response.context['latest_question_list'],
-            [question],
-        )
-
-    def test_future_question(self):
-        """
-        Questions with a pub_date in the future aren't displayed on
-        the index page.
-        """
-        create_question(question_text="Future question.", days=30)
-        response = self.client.get(reverse('polls:index'))
-        self.assertContains(response, "No polls are available.")
-        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+        past_question = create_question(question_text='Past Question.', days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
 
     def test_future_question_and_past_question(self):
         """
